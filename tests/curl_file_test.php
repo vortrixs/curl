d9ff6797ad78f38a2f211fcfcb82b6ed7e289a54
Hans Erik Jepsen <hanserikjepsen@hotmail.com>
2019-01-26 00:57:28 +0100

    Testing curl object consistency

--- tests/curl_file_test.php
+++ tests/curl_file_test.php
@@ -4,26 +4,35 @@ require __DIR__ . '/../Curl.php';
 
 use Vortrixs\Curl\Curl;
 
-$curl = new Curl('http://google.com');
-
-file_put_contents('./test.txt', 'This is a test file');
-
-$file = $curl->createCurlFile('test.txt', 'text/*', 'test_file');
-
-if (! ($file instanceof \CURLFile))
+try
 {
-	echo 'CURLFile corrupted';
-	var_dump($file, file_get_contents('./test.txt'));
+	$curl = new Curl('http://google.com');
+
+	file_put_contents('./test.txt', 'This is a test file');
+
+	$file = $curl->createCurlFile('test.txt', 'text/*', 'test_file');
+
+	if (! ($file instanceof \CURLFile))
+	{
+		echo 'CURLFile corrupted';
+		var_dump($file, file_get_contents('./test.txt'));
+		unlink('./test.txt');
+		exit(1);
+	}
+
+	$curl->options([CURLOPT_POST => true, CURLOPT_POSTFIELDS => ['file' => $file]]);
+
+	var_dump($curl->handle());
+
+	var_dump($curl);
+	//$curl->close();
+
 	unlink('./test.txt');
+
+	exit(0);
+}
+catch (\Throwable $t)
+{
+	echo $t;
 	exit(1);
 }
-
-$curl->options([CURLOPT_POST => true, CURLOPT_POSTFIELDS => ['file' => $file]]);
-
-var_dump($curl->handle());
-
-$curl->close();
-
-unlink('./test.txt');
-
-exit(0);
--- tests/curl_test.php
+++ tests/curl_test.php
@@ -4,24 +4,33 @@ require __DIR__ . '/../Curl.php';
 
 use Vortrixs\Curl\Curl;
 
-$exit = 0;
-
-$curl = new Curl('http://google.com');
-
-var_dump($curl->version());
-
-$curl->options([CURLOPT_RETURNTRANSFER => true, CURLOPT_FOLLOWLOCATION => true]);
-
-$result = $curl->execute();
-
-if (false === $result)
+try
 {
-	var_dump($curl->error(), $curl->errno());
+	$exit = 0;
+
+	$curl = new Curl('http://google.com');
+
+	var_dump($curl->version());
+
+	$curl->options([CURLOPT_RETURNTRANSFER => true, CURLOPT_FOLLOWLOCATION => true]);
+
+	$result = $curl->execute();
+
+	if (false === $result)
+	{
+		var_dump($curl->error(), $curl->errno());
+		$exit = 1;
+	}
+
+	var_dump($curl->info(CURLINFO_HTTP_CODE));
+
+	var_dump($curl);
+	//$curl->close();
+}
+catch(\Throwable $t)
+{
+	echo $t;
 	$exit = 1;
 }
-
-var_dump($curl->info(CURLINFO_HTTP_CODE));
-
-$curl->close();
 
 exit($exit);
